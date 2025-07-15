d4f682f7bd32528fa31f91ee07043b7aefa7ae6d
Peter Grigor <petrupewdiepie@gmail.com>
2025-07-15 20:31:43 +0300

    some changes

--- veo3/Views/GeneratedVideoDetailView.swift
+++ veo3/Views/GeneratedVideoDetailView.swift
@@ -72,6 +72,7 @@ struct GeneratedVideoDetailView: View {
                     }
                 }
                 .padding()
+                .padding(.top)
                 
                 // Video player or placeholder
                 ZStack {
